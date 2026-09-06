5ffc02f85e88d4d7f3bfa0734aa72a976b7850e2
Devulapalli Yugandhar Reddy <yugandhar1232006@gmail.com>
2026-09-06 08:57:48 +0530

    Add Hard C++ Solution for 115. Distinct Subsequences from LeetCode

--- LeetCode/Patterns/Dynamic Programming/115. Distinct Subsequences/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/115. Distinct Subsequences/Solution.cpp
@@ -1,1 +1,20 @@
-rabbbit
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        const long long LIMIT = INT_MAX;
+
+        vector<long long> dp(m + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[j] = min(LIMIT, dp[j] + dp[j - 1]);
+                }
+            }
+        }
+
+        return (int)dp[m];
+    }
+};
