74cfd64ddfdf5338a5ac76b0db0f84789aff1bac
Devulapalli Yugandhar Reddy <yugandhar1232006@gmail.com>
2026-09-10 09:14:12 +0530

    Add Medium C++ Solution for 2265. Count Nodes Equal to Average of Subtree from LeetCode

---
+++ LeetCode/Patterns/Trees/2265. Count Nodes Equal to Average of Subtree/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int ans = 0;
+
+    pair<int, int> dfs(TreeNode* root) {
+        if (!root) return {0, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int sum = root->val + left.first + right.first;
+        int count = 1 + left.second + right.second;
+
+        if (root->val == sum / count)
+            ans++;
+
+        return {sum, count};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+};
