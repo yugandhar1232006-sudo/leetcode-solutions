5b8d91605bea555b4cf463934f0e58cece397fcf
Devulapalli Yugandhar Reddy <yugandhar1232006@gmail.com>
2026-09-24 20:07:48 +0530

    Add Easy C++ Solution for 3550. Smallest Index With Digit Sum Equal to Index from LeetCode

--- LeetCode/Patterns/Arrays/3550. Smallest Index With Digit Sum Equal to Index/Solution.cpp
+++ LeetCode/Patterns/Arrays/3550. Smallest Index With Digit Sum Equal to Index/Solution.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) {
-            int n = nums[i];
-            int sum = 0;
+            int n = nums[i], sum = 0;
 
             while (n > 0) {
                 sum += n % 10;
